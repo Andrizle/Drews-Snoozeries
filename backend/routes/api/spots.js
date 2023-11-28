218900e01ed35199ee8bead0cf99e08937593235
Andrew <andrew.madrigal23@gmail.com>
2023-11-28 10:25:26 -0800

    finished get all spots of current user and get details of spot from id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { Op } = require('sequelize');
+const { Op, json } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
@@ -10,7 +10,7 @@ const spot = require('../../db/models/spot');
 
 const router = express.Router();
 
-router.get('/',  async (req, res, next) => {
+const defaultSpots = async (req, res, next) => {
     const spots = []
     const preSpots = await Spot.findAll();
 
@@ -30,11 +30,8 @@ router.get('/',  async (req, res, next) => {
 
         const avgRating = reviewsSum / reviewsCount;
 
-        const previewImage = await SpotImage.findAll({
-            attributes: ['url'],
-            where: {
-                spotId: spot.id
-            }
+        const previewImage = await SpotImage.findByPk(spot.id, {
+            attributes: ['url']
         })
 
         spot = spot.toJSON();
@@ -44,7 +41,95 @@ router.get('/',  async (req, res, next) => {
         spots.push(spot)
     }
 
-    res.json({spots})
+    return res.json({spots})
+}
+
+router.get('/', defaultSpots);
+
+router.get('/current', async (req, res, next) => {
+    const { user } = req;
+      if (user) {
+        const spots = []
+        const preSpots = await Spot.findAll({
+            where: {
+                ownerId: user.id
+            }
+        });
+
+        for (let i = 0; i < preSpots.length; i++) {
+            let spot = preSpots[i];
+            const reviewsSum = await Review.sum( 'stars', {
+                where: {
+                    spotId: spot.id
+                }
+            });
+
+            const reviewsCount = await Review.count({
+                where: {
+                    spotId: spot.id
+                }
+            });
+
+            const avgRating = reviewsSum / reviewsCount;
+
+            const previewImage = await SpotImage.findByPk(spot.id, {
+                attributes: ['url']
+            })
+
+            spot = spot.toJSON();
+            spot.avgRating = avgRating;
+            spot.previewImage = previewImage;
+
+            spots.push(spot)
+        }
+
+        return res.json({spots})
+    } else {
+        return res.status(401).json({"message": "Authentication required"})
+    }
+});
+
+router.get('/:spotId', async (req, res, next) => {
+    const { spotId } = req.params;
+
+    let spot = await Spot.findByPk(spotId);
+       if (spot) {
+    const reviewsSum = await Review.sum( 'stars', {
+        where: {
+            spotId
+        }
+    });
+
+    const reviewsCount = await Review.count({
+        where: {
+            spotId
+        }
+    });
+
+    const avgRating = reviewsSum / reviewsCount;
+
+    spot = spot.toJSON();
+
+    spot.numReviews = await Review.count({
+        where: {
+            spotId
+        }
+    })
+    spot.avgRating = avgRating;
+    spot.SpotImages = await SpotImage.findAll({
+        where: {
+            spotId
+        },
+        attributes: ['id', 'url', 'preview']
+    })
+    spot.Owner = await User.findByPk(spot.ownerId,{
+        attributes: ['id', 'firstName', 'lastName']
+    });
+
+    return res.json(spot)
+} else { return res.status(404).json({"message": "Spot couldn't be found"})}
+
 })
+
 
 module.exports = router;
