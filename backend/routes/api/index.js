5defb54dc820d500f1731fa297db0671e1a6bd87
Andrew <andrew.madrigal23@gmail.com>
2023-11-29 14:06:33 -0800

    finished delete a spot image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js')
+const spotImageRouter = require('./spotImages.js')
 const { setTokenCookie, restoreUser } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
@@ -15,6 +16,8 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 
 router.use('/reviews', reviewRouter);
+
+router.use('/spot-images', spotImageRouter)
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
