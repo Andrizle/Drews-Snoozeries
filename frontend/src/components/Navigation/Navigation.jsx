06529827a56687f47fa1e457313b5e339de46001
Andrew <andrew.madrigal23@gmail.com>
2024-01-01 23:14:49 -0800

    added finishing touches

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,13 +2,16 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from '../../../public/favicon-32x32.png'
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
     <div className='navBar'>
-        <NavLink className='navLink' id='homeButton' to="/">Home</NavLink>
+        <NavLink id='homeButton' to="/">
+          <img src={logo} id='webLogo' />
+        </NavLink>
         <div className='navRightSide'>
           <NavLink className='navLink' to="/spots/new">Create A New Spot</NavLink>
           <div>
